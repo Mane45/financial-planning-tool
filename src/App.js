6c65b435605632ab45f6ad2c3dc57b7ad3bd11ae
Narek003 <na.2003.er@gmail.com>
2022-12-24 13:42:30 +0400

    update changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          new changes
+          new changes 2
         </a>
       </header>
     </div>
