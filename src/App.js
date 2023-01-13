ec15937c0065052bb012534c50274e89c66b09ac
DESKTOP-J1T8E70\davs <Davit.sahakyan@volo.global>
2023-01-13 22:48:54 +0400

    refactored project and added Registration, functional and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import Manual from './pages/private/manual/Manual';
+import Summary from './pages/private/summary/Summary';
+import HomeLayout from './pages/private/homeLayout/HomeLayout';
+import Signup from './pages/public/signup/Signup';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import Login from './components/Login/Login';
+import Signin from './pages/public/signin/Signin';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>                            
+        <Routes>   
+          <Route path="/home" element={<HomeLayout/>}>
+            <Route index element={<Summary/>}/> 
+            <Route path='summary' element={<Summary/>}/> 
+            <Route path='manual' element={<Manual/>}/>
+          </Route>                                                                    
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin />}/>
+        </Routes>                    
+    </Router>
   );
 }
 
---
+++ src/pages/public/signup/Signup.jsx
@@ -0,0 +1,115 @@
+import React  from 'react';
+import {Link} from "react-router-dom";
+import { NavLink, useNavigate } from 'react-router-dom';
+
+import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import { auth } from '../../../firebase';
+import { firestore } from "../../../firebase";
+import { addDoc, collection } from "@firebase/firestore";
+import { Formik } from 'formik';
+
+import logo from "../../../assets/images/logo.png";
+import './Signup.css';
+
+const Login = () => {
+    const ref = collection(firestore, "users");
+    const navigate = useNavigate();
+
+
+return ( <div className="loginPage">
+            <div className='picture'></div>
+            <div className='loginContainer'>
+                <div className='loginContent'>
+                    <div className='logoContainer'>
+                        <img src={logo} alt="logo"></img>
+                    </div>
+                    <Formik
+                        initialValues={{ email: '', password: '', confirmPassword: '' }}
+                        validate={values => {
+                            const errors = {};
+                            if (!values.email) {
+                                errors.email = 'Required';
+                            } else if (
+                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                            ) {
+                                errors.email = 'Invalid email address';
+                            }
+                            return errors;
+                        }}
+                        onSubmit={async (values, { setSubmitting }) => {
+                            setSubmitting(false);
+                            await createUserWithEmailAndPassword(auth, values.email, values.password)
+                            .then((userCredential) => {
+                                const user = userCredential.user;
+                                console.log(user);
+                                navigate("/signin")
+                            })
+                            .catch((error) => {
+                                const errorCode = error.code;
+                                const errorMessage = error.message;
+                                console.log(errorCode, errorMessage);
+                            });
+                        }}
+                        >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting,
+                        }) => (
+                            <form onSubmit={handleSubmit} className="loginForm">
+
+                                <label>Login</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.email}
+                                    className="inputStyle"
+                                />
+                                {errors.email && touched.email && errors.email}
+
+
+                                <label>Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.password}
+                                    className="inputStyle"
+                                />
+                                {errors.password && touched.password && errors.password}
+
+                                <label>Confirm Password</label>
+                                <input
+                                    type="password"
+                                    name="confirmPassword"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.confirmPassword}
+                                    className="inputStyle"
+                                />
+                                {errors.confirmPassword && touched.confirmPassword && errors.confirmPassword}
+
+
+                                <button type="submit" disabled={isSubmitting} className="loginSubmit">
+                                    Register
+                                </button>
+                                <hr className='line'/>
+                                <div>
+                                    <span>Already have an account? <Link to={'/signin'}>signin</Link></span>
+                                </div>
+                            </form>
+                        )}
+                    </Formik>
+                </div>
+            </div>
+         </div> )
+};
+
+export default Login;
