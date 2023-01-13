2cd60244a4125a15e92f9f612e4734ac908d8545
DESKTOP-J1T8E70\davs <Davit.sahakyan@volo.global>
2023-01-13 22:48:54 +0400

    refactored project and added Registration, functional and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import Manual from './pages/private/manual/Manual';
+import Summary from './pages/private/summary/Summary';
+import HomeLayout from './pages/private/homeLayout/HomeLayout';
+import Signup from './pages/public/signup/Signup';
+import { BrowserRouter as Router} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import Login from './components/Login/Login';
+import Signin from './pages/public/signin/Signin';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
       <Login />
   gggggggddddhhhh
     </div>
+=======
+    <Router>                            
+        <Routes>   
+          <Route path="/home" element={<HomeLayout/>}>
+            <Route index element={<Summary/>}/> 
+            <Route path='summary' element={<Summary/>}/> 
+            <Route path='manual' element={<Manual/>}/>
+          </Route>                                                                    
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin />}/>
+        </Routes>                    
+    </Router>
+>>>>>>> ec15937 (refactored project and added Registration, functional and routing)
   );
 }
 
---
+++ src/pages/private/summary/Summary.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Summary.css";
+
+function Summary() {
+  return (
+    <div>
+        this is Summary
+    </div>
+  );
+}
+
+export default Summary;
